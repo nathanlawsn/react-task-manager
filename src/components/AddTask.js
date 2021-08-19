413353305c553ba42c7448d77d9cfa6b7d8a1a78
Nathan Lawson <nathan@consultanddesign.com>
2021-08-19 17:30:43 +0100

    Project Overlay and Task Date components

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,8 @@ import { FaRegListAlt, FaRegCalendarAlt } from 'react-icons/fa';
 import moment from 'moment';
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
+import { ProjectOverlay } from './ProjectOverlay';
+import { TaskDate } from './TaskDate';
 
 export const AddTask = ({
   showAddTaskMain = true,
@@ -103,6 +105,16 @@ export const AddTask = ({
               </div>
             </>
           )}
+          <ProjectOverlay
+            setProject={setProject}
+            showProjectOverlay={showProjectOverlay}
+            setShowProjectOverlay={setShowProjectOverlay}
+          />
+          <TaskDate
+            setTaskDate={setTaskDate}
+            showTaskDate={showTaskDate}
+            setShowTaskDate={setShowTaskDate}
+          />
           <input
             className="add-task__content"
             aria-label="Enter your task"
